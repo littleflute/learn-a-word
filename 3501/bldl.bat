b8fa28186cb2d62200a37931b1f01a59a978ed5b
littleflute <littleflute@163.com>
2021-01-06 17:25:31 -0800

    3552

--- 3501/bldl.bat
+++ 3501/bldl.bat
@@ -1,6 +1,9 @@
 
-curl https://raw.githubusercontent.com/littleflute/bat/master/bat/bldl.bat	    		-o 		bldl.bat
-curl https://raw.githubusercontent.com/littleflute/bat/master/bat/ffmpeg/srt2v.bat	    	-o 		srt2v.bat
-curl https://raw.githubusercontent.com/littleflute/bat/master/bat/ffmpeg/gif2v.bat	    	-o 		gif2v.bat
+curl https://raw.githubusercontent.com/littleflute/bat/master/bat/bldl.bat	    		                            -o 		bldl.bat
+curl https://raw.githubusercontent.com/littleflute/bat/master/bat/ffmpeg/srt2v.bat	    	                        -o 		srt2v.bat
+curl https://raw.githubusercontent.com/littleflute/bat/master/bat/ffmpeg/gif2v.bat	    	                        -o 		gif2v.bat
+curl https://raw.githubusercontent.com/littleflute/bat/master/bat/ffmpeg/logo2v.bat	                              	-o 		logo2v.bat
 
-curl https://raw.githubusercontent.com/littleflute/blog/master/i/i1250/1138.gif		    	-o 		1138.gif
+curl https://raw.githubusercontent.com/littleflute/blog/master/i/i1250/1138.gif		                            	-o 		1138.gif
+
+curl https://user-images.githubusercontent.com/17950965/103499626-9f52cb00-4dfd-11eb-9088-56e3fef3dd03.png	    	-o 		dt01.png
